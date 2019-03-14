67cfae0e3645f74e024c3a6fc33179f630097e92
lockeas16 <lockeas16@gmail.com>
2019-03-14 13:57:21 -0600

    done arrow functions have lexical , no dynamic

--- spec/koans.js
+++ spec/koans.js
@@ -370,12 +370,14 @@ describe('arrow functions. ', () => {
 
     getFunction() {
       return () => {
-        return new LexicallyBound(); /*changes might go here*/
+        // en lugar de devolver un nuevo objeto, se devuelve el objeto mismo con this?
+        return this;
       };
     }
 
     getArgumentsFunction() {
-      return function() { return arguments; }; /*or here*/
+      // se cambia por un arrow function, porque era una funcion normal
+      return () => { return arguments; }; 
     }
   }
 
@@ -385,23 +387,24 @@ describe('arrow functions. ', () => {
       let bound = new LexicallyBound();
       let fn = bound.getFunction();
 
-      //expect(fn()).toBe(bound);
+      expect(fn()).toBe(bound);
     });
 
     it('can NOT bind a different context', function() {
       let bound = new LexicallyBound();
       let fn = bound.getFunction();
       let anotherObj = {};
-      let expected = anotherObj; //change this
+      // lo esperado es el mismo objeto ligado con this?
+      let expected = bound; 
 
-      //expect(fn.call(anotherObj)).toBe(expected);
+      expect(fn.call(anotherObj)).toBe(expected);
     });
 
     it('`arguments` doesnt work inside arrow functions', function() {
       let bound = new LexicallyBound();
       let fn = bound.getArgumentsFunction();
 
-      //expect(fn(1, 2).length).toEqual(0);
+      expect(fn(1, 2).length).toEqual(0);
     });
 
   });
